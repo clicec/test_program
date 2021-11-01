ea467186972c894e80a5475c3718706c5370e427
clicec <cwquintal@gmail.com>
2021-11-01 13:09:06 -0700

    naming changes and service edits

--- kilBase.py
+++ kilBase.py
@@ -1,17 +1,26 @@
 from PySide6.QtCore import QTime, QTimer, Slot
 from PySide6.QtWidgets import QApplication, QLabel, QLineEdit
 from PySide6.QtWidgets import QPushButton, QMainWindow, QVBoxLayout, QWidget
+
 import rpyc
+from rpyc.core.protocol import Connection
+
 import logging
 import sys
 
-DefaultLog = 'test_program.log'
-DefaultName = 'Test Program'
+DefaultLog = 'kill_base.log'
+DefaultName = 'Kill Base'
 DefaultHostname = 'localhost'
 DefaultPort = 50555
-DefaultDebugStart = 'test_program Start'
+DefaultDebugStart = 'kill_base Start'
 
 logging.basicConfig(filename=DefaultLog, level=logging.INFO)
+	
+class KillException(Exception):
+	pass
+	
+class KillClientNotConnected(KillException):
+	pass
 	
 	
 class ClientService(rpyc.Service):
@@ -19,46 +28,64 @@ class ClientService(rpyc.Service):
 	DebugConnectError = 'ClientService Connection Error'
 	DebugPingServerError = 'ClientService ping_server Error'
 	
-	def __init__(self):
+	NeverConnected = 'Never Connected'
+	Connected = 'Connected'
+	NotConnected = 'Not Connected'
+	
+	def __init__(self, name=DefaultName):
 		self.connection = None
-		self.connected = False
-		self.name = DefaultName
+		self.status = self.NeverConnected
+		self.name = name
 		logging.debug(self.DebugInitStart)
 		
-	def my_connect():
-		result = False
+	def my_connect(self):
 		try:
 			self.connection = rpyc.connect(DefaultHostname, DefaultPort, service = self)
-			result = True
-		except Error as error:
+			self._check_status()
+		except Exception as error:
 			logging.error(self.DebugConnectError, error)
-		return result
-	
-	def on_connect():
-		self.connected = True
 		
-	def on_disconnect():
-		self.connected = False
+	def is_closed(self):
+		return self.connection is not Connection or self.connection.closed
+	
+	def _check_status(self):
+		if self.is_closed():
+			if self.status==self.Connected:
+				self.status = self.NotConnected
+		else:
+			if self.status != self.Connected:
+				self.status = self.Connected
+				
+	def get_status(self):
+		self._check_status()
+		return self.status
+			
+	def on_connect(self):
+		pass
+		
+	def on_disconnect(self):
+		pass
 		
 	def exposed_check(self, name):
-		return name == Name
+		return name == self.name
 		
-	def ping_server():
-		result = (self.connected or self.connection is None)
-		if not result:
-			result = my_connect()
+	def ping_server(self):
+		result = not self.is_closed()
 		if result:
 			try:
-				result = self.connection.root.ping()
-			except Error as error:
+				self.connection.root.ping()
+			except Exception as error:
+				result = False
 				logging.error(self.DebugPingServerError, error)
-		self.connected = result
 		return result
 
+
 class ClientControl:
-	def __init__(self, model, view):
-		self.my_model = model
-		self.my_view = view
+	def __init__(self, app):
+		self.my_app = app
+		self.my_service = ClientService()
+		self.my_model = ClientModel(self.my_service)
+		self.my_view = ClientView(self.my_model, self.my_app)
 
 
 class ClientModel:
@@ -80,8 +107,8 @@ class ClientModel:
 	def get_start_time(self):
 		return self.start_time
 		
-	def get_service_status():
-		return self.service.connected:
+	def get_service_status(self):
+		return self.my_service.get_status()
 	
 					
 class ClientView(QMainWindow):
@@ -118,6 +145,8 @@ class ClientView(QMainWindow):
 		self.start_label.setText(self.get_start_label())
 		self.last_label.setText(self.get_last_label())
 		self.status_label.setText(self.get_status_label())
+		self.status_label.adjustSize()
+
 		
 	def get_name_label(self):
 		return 'Program: ' + DefaultName
@@ -129,12 +158,7 @@ class ClientView(QMainWindow):
 		return 'Last Updated: ' + self.get_time_label(self.my_model.get_time())
 
 	def get_status_label(self):
-		result = 'Connection Status: '
-		if my_model.is_service_connected:
-			result += 'Connected'
-		else:
-			result += 'Not Connected'
-		return result
+		return 'Connection Status: ' + self.my_model.get_service_status()
 		
 	def get_time_label(self, time):
 		return time.toString(self.TimeFormat)
@@ -153,11 +177,11 @@ class ClientView(QMainWindow):
 def main():
 	logging.info(DefaultDebugStart)
 	my_app = QApplication(sys.argv)
-	my_model = ClientModel()
-	my_view = ClientView(my_model, my_app)
-	my_control = ClientControl(my_model, my_view)
+	my_control = ClientControl(my_app)
+	my_service = my_control.my_service
+	my_model = my_control.my_model
+	my_view = my_control.my_view
 	my_view.show()
-	my_service = ClientService()
 	sys.exit(my_app.exec())
 
 if __name__ == '__main__':
